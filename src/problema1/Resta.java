0d6c171938c39241e033c7f385126e6b5896789e
sergi <sergi@DESKTOP-3DDPO4F>
2018-02-02 02:00:11 +0100

    Problema 1

---
+++ src/problema1/Resta.java
@@ -0,0 +1,8 @@
+package problema1;
+
+public class Resta extends Operacion {
+
+	void operar() {
+		resultado = valor1 - valor2;
+	}
+}
---
+++ src/problema1/Suma.java
@@ -0,0 +1,8 @@
+package problema1;
+
+public class Suma extends Operacion {
+
+	void operar() {
+		resultado = valor1 + valor2;
+	}
+}
